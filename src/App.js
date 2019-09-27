6204d4d986d8621bf407251c477570366875f759
BobGizzmo <msi03@laposte.net>
2019-09-27 13:34:59 +0200

    React hooks commit one

--- src/App.js
+++ src/App.js
@@ -1,65 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import shuffle from 'lodash.shuffle'
 import './App.css';
 import Keyboard from './Keyboard';
 
-class App extends Component {
+const App =  () => {
 
-  keyboard = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+  const keyboard = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
             'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   
-  words = ['POMME', 'POIRE', 'PRUNE', 'PATATE', 'PAMPLEMOUSSE', 'POULE', 'PLAT', 'PIRATE']
+  const words = ['POMME', 'POIRE', 'PRUNE', 'PATATE', 'PAMPLEMOUSSE', 'POULE', 'PLAT', 'PIRATE']
 
-  state = {
-    word: shuffle(this.words)[0],
-    usedLetters: [],
-  }
+  const [word, setWord] = useState(shuffle(words)[0]);
+  const [usedLetters, addNewLetters] = useState([]);
 
   //Arrow function for binding
-  initState = () => {
-    this.setState({
-      word: shuffle(this.words)[0],
-      usedLetters: []
-    })
+  const initState = () => {
+    setWord(shuffle(words)[0]);
+    addNewLetters([]);
   }
 
   // Produit une représentation textuelle de l’état de la partie,
   // chaque lettre non découverte étant représentée par un _underscore_.
-  computeDisplay(phrase, usedLetters) {
+  const computeDisplay = (phrase, usedLetters) => {
     return phrase.replace(/\w/g,
       (letter) => (usedLetters.includes(letter) ? letter : '_ ')
     )
   }
 
   //Arrow function for binding
-  addLetters = (letter) => {
-    const { usedLetters, word} = this.state
-    usedLetters.push(letter);
+  const addLetter = letter => {
+    console.log(usedLetters)
+    const newLettersArray = [...usedLetters];
+    newLettersArray.push(letter);
 
-    this.setState({usedLetters: usedLetters})
-    this.computeDisplay(word, usedLetters)
+    addNewLetters(newLettersArray);
+    console.log(usedLetters)
+    computeDisplay(word, usedLetters);
   }
 
-  render() {
-    const { word, usedLetters } = this.state
-    const won = this.computeDisplay(word, usedLetters).indexOf('_') > -1 ? false : true
+    const won = computeDisplay(word, usedLetters).indexOf('_') > -1 ? false : true
     return (
       <section className="App">
         <header className="App-header">
           <h1>The Hangman Game</h1>
         </header>
         <div className="game">
-          <p>{this.computeDisplay(word, usedLetters)}</p>
+          <p>{computeDisplay(word, usedLetters)}</p>
           <div className="keyboard">
             {
               won ?
                 <p>Bravo tu as gagné en {usedLetters.length} coups !<br />
-                <button onClick={this.initState}>REJOUER</button></p> 
+                <button onClick={initState}>REJOUER</button></p> 
               :
-                this.keyboard.map((letter, index) => (
+                keyboard.map((letter, index) => (
                   <Keyboard keyboard={letter}
                     key={index}
-                    onclick={this.addLetters} />
+                    onclick={addLetter} />
                 ))
             }
           </div>
@@ -67,6 +63,5 @@ class App extends Component {
       </section>
     );
   }
-}
 
 export default App;
